9902c4e9f45fe3b9869fff079462b02effcb6336
Gianluck <giancetpro@gmail.com>
2025-06-22 15:58:42 -0500

    rico

---
+++ app/Livewire/DetalleConv.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class DetalleConv extends Component
+{
+    public function render()
+    {
+        return view('livewire.detalle-conv');
+    }
+}
